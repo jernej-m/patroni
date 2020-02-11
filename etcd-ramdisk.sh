710c54610644a638160619fa02b5053c0c6351c1
Cloud User <centos@ip-172-31-30-118.eu-central-1.compute.internal>
2020-02-11 11:08:04 +0000

    added etcd-ramdisk.sh

---
+++ etcd-ramdisk.sh
@@ -0,0 +1,57 @@
+echo "Updating yum repositories..."
+sleep 5
+yum -y update
+
+#-------------- Install etcd --------------------
+echo "Installing etcd..."
+sleep 5
+yum -y install etcd
+
+#---------- create ramdisk ----------------
+mkdir /mnt/ramdisk
+mount -t tmpfs -o size=1024m tmpfs /mnt/ramdisk
+
+# --------- Get variables ------------
+read -p "member 1 ip: " v_ip_1
+read -p "member 2 ip: " v_ip_2
+read -p "member 3 ip: " v_ip_3
+read -p "Enter this member number (1,2,3): " v_member_no
+
+if [ $v_member_no == 1 ]
+then
+   v_this_ip=$v_ip_1
+elif [ $v_member_no == 2 ]
+then
+   v_this_ip=$v_ip_2
+elif [ $v_member_no == 3 ]
+then
+   v_this_ip=$v_ip_3
+else
+   echo "Wrong cluster number provided"
+fi
+
+# --------- Edit etcd configuration file -----------
+echo "Editing etcd configuration file..."
+sleep 5
+
+sed -i "s/#\?ETCD_NAME=\".*\"/ETCD_NAME=\"etcd_$v_member_no\"/g" /etc/etcd/etcd.conf
+sed -i "s/#\?ETCD_LISTEN_PEER_URLS=\".*\"/ETCD_LISTEN_PEER_URLS=\"http:\/\/0.0.0.0:2380\"/g" /etc/etcd/etcd.conf
+sed -i "s/#\?ETCD_LISTEN_CLIENT_URLS=\".*\"/ETCD_LISTEN_CLIENT_URLS=\"http:\/\/0.0.0.0:2379\"/g" /etc/etcd/etcd.conf
+sed -i "s/#\?ETCD_INITIAL_ADVERTISE_PEER_URLS=\".*\"/ETCD_INITIAL_ADVERTISE_PEER_URLS=\"http:\/\/$v_this_ip:2380\"/g" /etc/etcd/etcd.conf
+sed -i "s/#\?ETCD_INITIAL_CLUSTER=\".*\"/ETCD_INITIAL_CLUSTER=\"etcd_1=http:\/\/$v_ip_1:2380,etcd_2=http:\/\/$v_ip_2:2380,etcd_3=http:\/\/$v_ip_3:2380\"/g" /etc/etcd/etcd.conf
+sed -i "s/#\?ETCD_INITIAL_CLUSTER_STATE=\".*\"/ETCD_INITIAL_CLUSTER_STATE=\"new\"/g" /etc/etcd/etcd.conf
+sed -i "s/#\?ETCD_INITIAL_CLUSTER_TOKEN=\".*\"/ETCD_INITIAL_CLUSTER_TOKEN=\"itclustertoken\"/g" /etc/etcd/etcd.conf
+sed -i "s/#\?ETCD_ADVERTISE_CLIENT_URLS=\".*\"/ETCD_ADVERTISE_CLIENT_URLS=\"http:\/\/$v_this_ip:2379\"/g" /etc/etcd/etcd.conf
+sed -i "s/#\?ETCD_WAL_DIR=\".*\"/ETCD_WAL_DIR=\"\/mnt\/ramdisk\/wal.etcd\"/g" /etc/etcd/etcd.conf
+
+# ---------- Start etcd service ---------------
+echo "Enabling etcd on start..."
+sleep 2
+systemctl enable etcd
+
+read -p "Start Etcd service (y/n)? it's recommended that Etcd service is started simultaneously on all instances." CONT
+if [ "$CONT" = "y" ]; then
+  systemctl start etcd
+else
+  echo "Etcd service start aborted.";
+fi
